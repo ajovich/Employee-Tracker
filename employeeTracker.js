0d100590a5e273e9d3a256856e9d968fec1de0a8
Allysha Jovich <ajovich@hotmail.com>
2021-06-09 14:28:37 -0700

    created runSearch function with rawlist user prompts, created switch case statements for each action

--- employeeTracker.js
+++ employeeTracker.js
@@ -12,3 +12,72 @@ const connection = mysql.createConnection({
     database: "employeeTracker_db",
 });
 
+connection.connect((err) => {
+    if (err) throw err;
+    runSearch();
+});
+
+const runSearch = () => {
+    inquirer
+      .prompt({
+        name: 'action',
+        type: 'rawlist',
+        message: 'What would you like to do?',
+        choices: [
+            'Add department',
+            'Add role',
+            'Add employee',
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Update employee role',
+            'Remove department',
+            'Remove role',
+            'Remove employee',
+        ],
+      })
+
+      .then((answer) => {
+          switch (answer.action) {
+            case 'Add department':
+                addDepartment();
+                break;
+
+            case 'Add role':
+                addRole();
+                break;
+
+            case 'Add employee':
+                addEmployee();
+                break;
+
+            case 'View all departments':
+                viewDepartments();
+                break;
+
+            case 'View all roles':
+                viewRoles();
+                break;
+
+            case 'View all employees':
+                viewEmployees();
+                break;
+
+            case 'Update employee role':
+                updateEmployeeRole();
+                break;
+
+            case 'Remove department':
+                removeDepartment();
+                break;
+
+            case 'Remove role':
+                removeRole();
+                break;
+
+            case 'Remove employee':
+                removeEmployee();
+                break; 
+          }
+      });
+};
